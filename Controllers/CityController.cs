7c0834d72dc528d780cb9ab2fb6313fa51220ff0
HendrikCromboom <hcromboom@gmail.com>
2020-12-05 14:03:22 +0100

    basic controller functionality with routing

---
+++ Controllers/CityController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")] //routing
+    [ApiController] // serves http response
+    public class CityController : ControllerBase
+    {
+        [HttpGet] // following function is a http get
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Atlanta", "New Yourk" };
+        }
+    }
+}
